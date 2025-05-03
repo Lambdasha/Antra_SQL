5a37f3e6601d9f3654f847832289dc80cd837e29
Lambdasha <shajingshu@gmail.com>
2025-05-03 01:01:08 -0700

    Update day3

--- day3.sql
+++ day3.sql
@@ -6,6 +6,7 @@ SELECT DISTINCT c.City
 FROM dbo.Customers c JOIN dbo.Employees e ON c.City = e.City
 
 -- 2. List all cities that have Customers but no Employee.
+
   -- a. Use sub-query
 SELECT DISTINCT c.City
 FROM dbo.Customers c
@@ -48,34 +49,96 @@ FROM dbo.Customers c JOIN dbo.Orders o ON c.CustomerID = o.CustomerID
 WHERE c.City != o.ShipCity
 
 --8. List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
-SELECT TOP(5) p.ProductID, p.ProductName, SUM(od.Quantity) AS TotalQuantity,
-AVG(od.UnitPrice) AS AveragePrice,
+WITH CityCTE AS
+(
+SELECT od.ProductID, c.City, SUM(od.Quantity) AS Quantity
+FROM dbo.Customers c JOIN dbo.Orders o ON c.CustomerID = o.CustomerID
+JOIN dbo.[Order Details] od ON o.OrderID = od.OrderID
+GROUP BY c.City, od.ProductID
+),
+MAXCityCTE AS
+(
+SELECT ProductID, City, Quantity
+FROM
+(
+SELECT ProductID, City, Quantity, MAX(Quantity) OVER (PARTITION BY ProductID) AS MaxQuantity
+FROM CityCTE
+) AS t
+WHERE Quantity = MaxQuantity
+)
 
-SELECT c.City, p.ProductID, MAX(SUM(od.Quantity))
+SELECT TOP 5 p.ProductID, p.ProductName, SUM(od.Quantity) AS TotalQuantity,
+AVG(od.UnitPrice) AS AveragePrice, 
+(
+  SELECT MAXCityCTE.City
+  FROM MAXCityCTE
+  WHERE p.ProductID = MAXCityCTE.ProductID
+) AS CustomerCity
 FROM dbo.Customers c JOIN dbo.Orders o ON o.CustomerID = c.CustomerID
 JOIN dbo.[Order Details] od ON od.OrderID = o.OrderID
 JOIN dbo.Products p ON p.ProductID = od.ProductID
-GROUP BY c.City, p.ProductID
-ORDER BY p.ProductID
-
-FROM dbo.Products p JOIN dbo.[Order Details] od ON p.ProductID = od.ProductID
-JOIN dbo.Orders o ON o.OrderID = od.OrderID
-JOIN dbo.Customers c ON o.CustomerID = c.CustomerID
 GROUP BY p.ProductID, p.ProductName
 ORDER BY SUM(od.Quantity) DESC
 
-SELECT c.City, p.ProductID, SUM(od.Quantity)
-FROM dbo.Customers c JOIN dbo.Orders o ON o.CustomerID = c.CustomerID
-JOIN dbo.[Order Details] od ON od.OrderID = o.OrderID
-JOIN dbo.Products p ON p.ProductID = od.ProductID
-GROUP BY c.City, p.ProductID
-
 --9. List all cities that have never ordered something but we have employees there.
 
---a. Use sub-query
+  --a. Use sub-query
+    -- If city means customer city:
+SELECT e.City 
+FROM dbo.Employees e
+WHERE e.City NOT IN 
+(SELECT DISTINCT c.City FROM dbo.Customers c WHERE c.CustomerID IN 
+  (SELECT DISTINCT o.CustomerID FROM dbo.Orders o)
+)
+    -- If city means shipcity:
+SELECT e.City 
+FROM dbo.Employees e
+WHERE e.City NOT IN 
+(SELECT DISTINCT o.ShipCity FROM dbo.Orders o)
 
---b. Do not use sub-query
+  --b. Do not use sub-query
+    -- If city means customer city: 
+SELECT DISTINCT e.City
+FROM dbo.Employees e LEFT JOIN dbo.Customers c ON e.City = c.City
+LEFT JOIN dbo.Orders o ON c.CustomerID = o.CustomerID 
+WHERE c.City IS NULL OR o.CustomerID IS NULL
+    -- If city means shipcity:
+SELECT DISTINCT e.City
+FROM dbo.Employees e LEFT JOIN dbo.Orders o ON e.City = o.ShipCity
+WHERE o.ShipCity IS NULL
 
 --10. List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
 
+WITH MostOrdersCTE AS
+(
+  SELECT TOP 1 e.City, Count(*) OrderNumber
+  FROM dbo.Employees e JOIN dbo.Orders o ON e.EmployeeID = o.EmployeeID
+  GROUP BY e.City
+  ORDER BY Count(*) DESC
+),
+MostQtyCTE AS
+(
+  SELECT TOP 1 c.City, SUM(od.Quantity) ProductQuantity
+  FROM dbo.Customers c JOIN dbo.Orders o ON c.CustomerID = o.CustomerID
+  JOIN dbo.[Order Details] od ON o.OrderID = od.OrderID
+  GROUP BY c.City
+  ORDER BY SUM(od.Quantity) DESC
+)
+SELECT MostOrdersCTE.city
+FROM MostOrdersCTE JOIN MostQtyCTE ON MostOrdersCTE.City = MostQtyCTE.City
+
 --11. How do you remove the duplicates record of a table?
+  -- Use partition by over all columns and add row number to find the duplicated ones.
+
+WITH RemoveDuplicates AS
+(
+SELECT *, 
+ROW_NUMBER() OVER (PARTITION BY ID, Name ORDER BY ID) AS rn
+FROM Q11_Sample
+)
+DELETE
+FROM RemoveDuplicates
+WHERE rn > 1
+
+
+
